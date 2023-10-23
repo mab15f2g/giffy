db61be01184ff886890574eafd9de83a85051612
Marcus <37145159+mab15f2g@users.noreply.github.com>
2023-10-23 08:41:27 +0200

    linting

--- giffy.py
+++ giffy.py
@@ -5,13 +5,21 @@ import tkinter as tk
 from tkinter import filedialog
 from tkinter.ttk import Progressbar  # Import Progressbar from ttk
 
-def komprimiere_video(datei_pfad, ziel_datei_pfad):
+
+def gif_erstellen(datei_pfad, ziel_datei_pfad):
+    """ Function to compress a video file to a gif
+        :param datei_pfad: Pfad zur Videodatei
+        :param ziel_datei_pfad: Pfad zur Zieldatei
+    """    
     print(f"Generiere GIF aus {datei_pfad}...")  # Display progress
     ziel_datei_pfad_gif = os.path.splitext(ziel_datei_pfad)[0] + '.gif'
-    kommando = ['ffmpeg', '-i', datei_pfad, '-vf', 'scale=640:-1:flags=lanczos,fps=20', ziel_datei_pfad_gif]
+    gif_filter = 'scale=640:-1:flags=lanczos,fps=20'
+    kommando = ['ffmpeg', '-i', datei_pfad, '-vf', gif_filter, ziel_datei_pfad_gif]
     subprocess.run(kommando, capture_output=True, text=True)
 
-def komprimiere_ordnerstruktur(quellpfad, zielpfad):
+
+def all_videos_to_gif(quellpfad, zielpfad):
+    """"Function to compress all videos in a directory"""
     for ordnername, unterordner, dateien in os.walk(quellpfad):
         # Erzeuge den Zielordner, wenn er nicht vorhanden ist
         zielordner = os.path.join(zielpfad, os.path.relpath(ordnername, quellpfad))
@@ -26,8 +34,8 @@ def komprimiere_ordnerstruktur(quellpfad, zielpfad):
             if os.path.exists(os.path.join(ordnername, datei)):
                 print(f"Die Zieldatei {ziel_datei_pfad} existiert bereits. Überspringe das Komprimieren.")
             else:
-                if datei.endswith(('.mp4', '.avi', '.mkv', '.mov', '.wmv', '.flv', '.webm')):  # Nur Dateien mit diesen Endungen komprimieren
-                    komprimiere_video(datei_pfad, ziel_datei_pfad)
+                if datei.endswith(('.mp4', '.avi', '.mkv', '.mov', '.wmv', '.flv', '.webm')):
+                    gif_erstellen(datei_pfad, ziel_datei_pfad)
                 else:
                     # Kopiere andere Dateien einfach in den Zielordner
                     shutil.copy2(datei_pfad, ziel_datei_pfad)
@@ -38,24 +46,29 @@ def komprimiere_ordnerstruktur(quellpfad, zielpfad):
             update_progress(prozent)
 
 def choose_source_directory():
+    """Function to choose the source directory"""
     source_dir = filedialog.askdirectory()
     source_dir_entry.delete(0, tk.END)
     source_dir_entry.insert(0, source_dir)
 
+
 def choose_destination_directory():
+    """Function to choose the destination directory"""
     dest_dir = filedialog.askdirectory()
     dest_dir_entry.delete(0, tk.END)
     dest_dir_entry.insert(0, dest_dir)
 
 # Function to update the progress bar
 def update_progress(progress_value):
+    """Function to update the progress bar"""
     progress_bar["value"] = progress_value
     app.update_idletasks()
 
 def compress_button_click():
+    """Function to start the compression"""
     source_dir = source_dir_entry.get()
     dest_dir = dest_dir_entry.get()
-    komprimiere_ordnerstruktur(source_dir, dest_dir)
+    all_videos_to_gif(source_dir, dest_dir)
 
 # Create the main application window
 app = tk.Tk()
@@ -67,7 +80,8 @@ source_label = tk.Label(app, text="Quellverzeichnis:")  # Source Directory in Ge
 source_label.pack()
 source_dir_entry = tk.Entry(app)
 source_dir_entry.pack()
-source_dir_button = tk.Button(app, text="Quellverzeichnis auswählen", command=choose_source_directory)  # Choose Source Directory in German
+# Choose Source Directory in German
+source_dir_button = tk.Button(app, text="Quellverzeichnis auswählen", command=choose_source_directory)
 source_dir_button.pack()
 
 # Add destination directory entry and button
@@ -75,11 +89,15 @@ dest_label = tk.Label(app, text="Zielverzeichnis:")  # Destination Directory in 
 dest_label.pack()
 dest_dir_entry = tk.Entry(app)
 dest_dir_entry.pack()
-dest_dir_button = tk.Button(app, text="Zielverzeichnis auswählen", command=choose_destination_directory)  # Choose Destination Directory in German
+
+# Add a button to choose the destination directory
+TEXT_ZIEL = "Zielverzeichnis auswählen"  # Choose Destination Directory in German
+dest_dir_button = tk.Button(app, text=TEXT_ZIEL, command=choose_destination_directory)
 dest_dir_button.pack()
 
+TEXT_ERSTELLEN = "Gifs erstellen"
 # Add compress button
-compress_button = tk.Button(app, text="Gifs erstellen", command=compress_button_click)  # Compress Videos in German
+compress_button = tk.Button(app, text=TEXT_ERSTELLEN, command=compress_button_click)
 compress_button.pack()
 
 # Add a Progressbar
